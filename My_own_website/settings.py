74b0daf369d4a68e77a0146a1151a8c721adec6d
Kristian <kristian.mirabile@gmail.com>
2020-09-05 00:04:56 +0000

    remove old stuff

--- My_own_website/settings.py
+++ My_own_website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '&jo^)a(5r7a**e2jwdbkt=maz%m6cpslat&-pr@anry8_$kqdx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kristianmirabile.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
